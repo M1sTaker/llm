dd7f7c5c3757e70b765c18a0a3c01bfc672e92c3
M1sTaker <69234606+M1sTaker@users.noreply.github.com>
2024-04-04 00:09:48 +0800

    Update train.py

--- train/llama/train.py
+++ train/llama/train.py
@@ -13,6 +13,11 @@ import logging
 import copy
 import json
 
+IGNORE_INDEX = -100
+DEFAULT_PAD_TOKEN = "[PAD]"
+DEFAULT_EOS_TOKEN = "</s>"
+DEFAULT_BOS_TOKEN = "</s>"
+DEFAULT_UNK_TOKEN = "</s>"
 PROMPT_DICT = {
     "prompt_input": (
         "Below is an instruction that describes a task, paired with an input that provides further context. "
@@ -51,6 +56,69 @@ class TrainingArguments(transformers.TrainingArguments):
         default=512,
         metadata={"help": "Maximum sequence length."}
     )
+
+def _tokenize_fn(
+    texts: Sequence[str],
+    tokenizer: LlamaTokenizer,
+    ) -> Dict:
+    """
+    对文本序列进行tokenize
+
+    Args:
+        texts (Sequence[str]): 文本序列
+        tokenizer (LlamaTokenizer)
+
+    Returns:
+        Dict: input_ids, labels, input_ids_lens, labels_lens
+    """
+    tokenized_list = [
+        tokenizer(
+            text=text,
+            return_tensors="pt",
+            padding="longest",
+            max_length=tokenizer.model_max_length,
+            truncation = True,
+        )
+        for text in texts
+    ]
+    input_ids = labels = [tokenized.input_ids[0] for tokenized in tokenized_list]
+    # 每个序列的真实长度
+    input_ids_lens = labels_lens = [
+        tokenized.input_ids.ne(tokenizer.pad_token_id).sum().item() for tokenized in tokenized_list
+    ]
+    return dict(
+        input_ids=input_ids,
+        labels=labels,
+        input_ids_lens=input_ids_lens,
+        labels_lens=labels_lens,
+    )
+    
+def preprocess(
+    sources: Sequence[str],
+    targets: Sequence[str],
+    tokenizer: LlamaTokenizer,
+    ) -> Dict:
+    """
+    使用tokenizer对文本进行tokenize
+
+    Args:
+        sources (Sequence[str]): 输入
+        targets (Sequence[str]): 输出
+        tokenizer (LlamaTokenizer): llama的tokenizer
+    """
+    examples = [s + t for s, t in zip(sources, targets)]
+    
+    examples_tokenized = _tokenize_fn(examples, tokenizer)
+    sources_tokenized = _tokenize_fn(sources, tokenizer)
+    
+    input_ids = examples_tokenized["input_ids"]
+    labels = copy.deepcopy(input_ids)
+    
+    for label, source_len in zip(labels, sources_tokenized["input_ids_lens"]):
+        label[:source_len] = IGNORE_INDEX
+    
+    return dict(input_ids=input_ids, labels=labels)
+    
     
 class SupervisedDataset(Dataset):
     """
@@ -71,10 +139,19 @@ class SupervisedDataset(Dataset):
             prompt_input.format_map(example) if example.get("input", "") != "" else prompt_no_input.format_map(example)
             for example in list_data_dict
         ]
-        targets = [f"{example["output"]}{tokenizer.eos_token}" for example in list_data_dict]
+        targets = [f"{example['output']}{tokenizer.eos_token}" for example in list_data_dict]
         
         logging.warning("Tokenizing inputs...")
         data_dict = preprocess(sources, targets, tokenizer)
+    
+        self.input_ids = data_dict["input_ids"]
+        self.labels = data_dict["labels"]
+    
+    def __len__(self):
+        return len(self.input_ids)
+    
+    def __getitem__(self, index) -> Dict[str, torch.Tensor]:
+        return dict(inpu_ids=self.input_ids[index], labels=self.labels[index])
         
 def prepare_supervised_data_module(tokenizer: LlamaTokenizer, data_args) -> Dict:
     """
@@ -97,7 +174,7 @@ def train():
     tokenizer = LlamaTokenizer.from_pretrained(
         model_args.model_name_or_path,
         cache_dir=training_args.cache_dir,
-        max_seq_len=training_args.max_seq_len,
+        model_max_len=training_args.max_seq_len,
         padding_side="right",
         use_fast=False,
     )
