e68bae0d34299cc8816f478e969ff6ed8b9ba51b
M1sTaker <69234606+M1sTaker@users.noreply.github.com>
2024-04-04 20:45:45 +0800

    updata launch.json

---
+++ train/llama/run_training.sh
@@ -0,0 +1,22 @@
+CUDA_VISIBLE_DEVICES=0 python train.py \
+    --model_name_or_path  Llama-2-7b-hf \
+    --data_path alpaca_data_cleaned.json \
+    --bf16 True \
+    --output_dir sft_7b \
+    --num_train_epochs 1 \
+    --per_device_train_batch_size 4 \
+    --per_device_eval_batch_size 4 \
+    --gradient_accumulation_steps 8 \
+    --evaluation_strategy "no" \
+    --save_strategy "steps" \
+    --save_steps 2000 \
+    --save_total_limit 1 \
+    --learning_rate 2e-5 \
+    --weight_decay 0. \
+    --warmup_ratio 0.03 \
+    --lr_scheduler_type "cosine" \
+    --logging_steps 1 \
+    --report_to "tensorboard" \
+    --fsdp "full_shard auto_wrap" \
+    --fsdp_transformer_layer_cls_to_wrap 'LlamaDecoderLayer' \
+    --tf32 True
